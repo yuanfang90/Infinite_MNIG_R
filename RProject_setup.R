cb2a197fbe87a10dc1c74a93ed0596caef952a8d
Yuan Fang <yfang8@binghamton.edu>
2020-10-22 13:41:03 -0400

    added project_Setup file

---
+++ RProject_setup.R
@@ -0,0 +1,14 @@
+###############################################################################
+#
+# Setup RProject Directories
+# created: 10/12/20202
+# author: Ariel Chernofsky
+#
+###############################################################################
+
+if(!file.exists("data")){dir.create("data")}
+if(!file.exists("R")){dir.create("R")}
+# if(!file.exists("SAS")){dir.create("SAS")}
+if(!file.exists("output")){dir.create("output")}
+if(!file.exists("output/plots")){dir.create("output/plots")}
+if(!file.exists("output/tables")){dir.create("output/tables")}
