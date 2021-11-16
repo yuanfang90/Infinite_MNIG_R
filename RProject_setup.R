645d7b2770cc9a87322ce3356aa18786061c70f8
yuanfang90 <yuanf@bu.edu>
2021-11-16 11:35:49 -0500

    Update RProject_setup.R

--- RProject_setup.R
+++ RProject_setup.R
@@ -1,13 +1,14 @@
 ###############################################################################
 #
 # Setup RProject Directories
-# created: 10/12/20202
+# created: 10/12/2020
 # author: Ariel Chernofsky
+# modified: Yuan Fang
 #
 ###############################################################################
 
 if(!file.exists("data")){dir.create("data")}
-if(!file.exists("R")){dir.create("R")}
+# if(!file.exists("R")){dir.create("R")}
 # if(!file.exists("SAS")){dir.create("SAS")}
 if(!file.exists("output")){dir.create("output")}
 if(!file.exists("output/plots")){dir.create("output/plots")}
